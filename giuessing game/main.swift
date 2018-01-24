b50e5b500162ae2ab8137fceabaa14073f3e7f16
Zemar Benitez <zemarbenitez@Interapts-MacBook-Pro-103.local>
2018-01-24 13:24:16 -0500

    outlined project

---
+++ giuessing game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  giuessing game
+//
+//  Created by Zemar Benitez on 1/23/18.
+//  Copyright © 2018 Zemar Benitez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
